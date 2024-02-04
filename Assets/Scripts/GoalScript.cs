16487562cf47e95b25001624beb40a8de03502f8
BuildTools <unconfigured@null.spigotmc.org>
2024-02-04 15:55:50 +0900

    タイマーの追加とステージの追加

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalScript : MonoBehaviour
 {
-    [SerializeField] private GameManager m_gameManager;
+    [SerializeField]
+    private GameManager m_gameManager;
+
+    [SerializeField]
+    private Text        m_text;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -12,7 +18,7 @@ public class GoalScript : MonoBehaviour
             if(!m_gameManager.GetGoal())
             {
                 m_gameManager.SetGoal(true);
-                Debug.Log("Goal");
+                m_text.gameObject.SetActive(true);
             }
         }
     }
