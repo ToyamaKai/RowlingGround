12c00b227ada3713c94e462028f7319ef5018cd5
BuildTools <unconfigured@null.spigotmc.org>
2024-02-15 01:03:05 +0900

    コンフリクト修正2

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -20,30 +20,18 @@ public class GoalScript : MonoBehaviour
 
     [SerializeField]
     private Text        m_text;
-<<<<<<< HEAD
-=======
-
->>>>>>> BaseSystem/Timer
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             if (!m_gameManager.GetGoal())
             {
-<<<<<<< HEAD
-                Debug.Log("Goal");
-=======
                 m_gameManager.SetGoal(true);
->>>>>>> BaseSystem/Timer
                 m_text.gameObject.SetActive(true);
                 StartCoroutine("ReturnTitle");
             }
         }
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> BaseSystem/Timer
     IEnumerator ReturnTitle()
     {
         yield return new WaitForSeconds(3);
