bb8f4a827df98fb7ee92961f3069d8179c27a5c9
BuildTools <unconfigured@null.spigotmc.org>
2024-02-15 00:37:26 +0900

    様々なものを追加

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GoalScript : MonoBehaviour
 {
+    private enum GoalType
+    {
+        Normal,
+        Kakushi
+    }
+
     [SerializeField]
     private GameManager m_gameManager;
+
+    [SerializeField]
+    GoalType m_goalType;
 
     [SerializeField]
     private Text        m_text;
@@ -19,7 +29,13 @@ public class GoalScript : MonoBehaviour
             {
                 m_gameManager.SetGoal(true);
                 m_text.gameObject.SetActive(true);
+                StartCoroutine("ReturnTitle");
             }
         }
+    }
+    IEnumerator ReturnTitle()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("TitleScene");
     }
 }
