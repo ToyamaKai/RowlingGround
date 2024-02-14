74186817c0b07aa81707603667e9d66116582746
BuildTools <unconfigured@null.spigotmc.org>
2024-02-15 00:36:54 +0900

    Goalスクリプトのコンフリクト修正

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -1,25 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-public class GoalScript : MonoBehaviour
-{
-    [SerializeField] private GameManager m_gameManager;
-=======
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GoalScript : MonoBehaviour
 {
-=======
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-
-public class GoalScript : MonoBehaviour
-{
->>>>>>> Stashed changes
     private enum GoalType
     {
         Normal,
@@ -34,27 +20,19 @@ public class GoalScript : MonoBehaviour
 
     [SerializeField]
     private Text        m_text;
-
->>>>>>> Stashed changes
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            if(!m_gameManager.GetGoal())
+            if (!m_gameManager.GetGoal())
             {
-                m_gameManager.SetGoal(true);
-<<<<<<< Updated upstream
                 Debug.Log("Goal");
-=======
                 m_text.gameObject.SetActive(true);
                 StartCoroutine("ReturnTitle");
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
             }
         }
     }
+
     IEnumerator ReturnTitle()
     {
         yield return new WaitForSeconds(3);
